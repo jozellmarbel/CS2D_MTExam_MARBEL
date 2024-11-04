cb39c3d5cf476f66db2ae318f8a5fcce6d8b2953
jozellmarbel <jozellmarbel@gmail.com>
2024-11-04 18:36:15 +0800

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullname = 'Jozell Marbel';
+let age = '19';
+let favNumber = '11';
+let favColor = 'Yellow'
+
+console.log = (fullname);
+console.log = (age);
+console.log = (favNumber);
+console.log = (favColor);
