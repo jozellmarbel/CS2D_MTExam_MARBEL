dbec31a13b4fb6c3974684277269a666615891eb
jozellmarbel <jozellmarbel@gmail.com>
2024-11-04 18:42:17 +0800

    Create classSubjects.js

---
+++ classSubjects.js
@@ -0,0 +1,7 @@
+let subTitle = "Art Appreciation";
+let classSched = "Monday/Friday 11:00 AM - 12:30 PM";
+let classroom = "Ched 121";
+let classInstructor = "Gem Marquez";
+let stuName = "Jozell Marbel";
+
+console.log(`${stuName} is currently enrolled in ${subTitle} with a class schedule of ${classSched} at room ${classroom}. The instructor for the subject is ${classInstructor}`);
